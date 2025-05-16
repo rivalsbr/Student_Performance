777d276a0045a5ea586e4126d01988c578c3e600
Muhammad Rivaldi <132969774+rivalsbr@users.noreply.github.com>
2025-05-16 15:49:09 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -4,13 +4,14 @@ import joblib
 import numpy as np
 import pandas as pd
 
-encoder_Tuition_fees_up_to_date = joblib.load("model/encoder_Tuition_fees_up_to_date.joblib")
-encoder_Scholarship_holder = joblib.load("model/encoder_Scholarship_holder.joblib")
-encoder_Debtor = joblib.load("model/encoder_Debtor.joblib")
-encoder_Displaced = joblib.load("model/encoder_Displaced.joblib")
-encoder_Daytime_evening_attendance = joblib.load("model/encoder_Daytime_evening_attendance.joblib")
 encoder_Gender = joblib.load("model/encoder_Gender.joblib")
+encoder_Debtor = joblib.load("model/encoder_Debtor.joblib")
+encoder_Daytime_evening_attendance = joblib.load("model/encoder_Daytime_evening_attendance.joblib")
+encoder_Scholarship_holder = joblib.load("model/encoder_Scholarship_holder.joblib")
+encoder_Tuition_fees_up_to_date = joblib.load("model/encoder_Tuition_fees_up_to_date.joblib")
+encoder_Displaced = joblib.load("model/encoder_Displaced.joblib")
 scaler_Admission_grade = joblib.load("model/scaler_Admission_grade.joblib")
+scaler_Previous_qualification_grade = joblib.load("model/scaler_Previous_qualification_grade.joblib")
 scaler_Curricular_units_1st_sem_approved = joblib.load("model/scaler_Curricular_units_1st_sem_approved.joblib")
 scaler_Curricular_units_1st_sem_credited = joblib.load("model/scaler_Curricular_units_1st_sem_credited.joblib")
 scaler_Curricular_units_1st_sem_enrolled = joblib.load("model/scaler_Curricular_units_1st_sem_enrolled.joblib")
@@ -19,7 +20,6 @@ scaler_Curricular_units_2nd_sem_approved = joblib.load("model/scaler_Curricular_
 scaler_Curricular_units_2nd_sem_credited = joblib.load("model/scaler_Curricular_units_2nd_sem_credited.joblib")
 scaler_Curricular_units_2nd_sem_enrolled = joblib.load("model/scaler_Curricular_units_2nd_sem_enrolled.joblib")
 scaler_Curricular_units_2nd_sem_grade = joblib.load("model/scaler_Curricular_units_2nd_sem_grade.joblib")
-scaler_Previous_qualification_grade = joblib.load("model/scaler_Previous_qualification_grade.joblib")
 
 def data_preprocessing(data):
     """Preprocessing data
@@ -34,12 +34,13 @@ def data_preprocessing(data):
     df = pd.DataFrame()
 
     # Perform transformations
-    df["Tuition_fees_up_to_date"] = encoder_Tuition_fees_up_to_date.transform(data["Tuition_fees_up_to_date"])
-    df["Scholarship_holder"] = encoder_Scholarship_holder.transform(data["Scholarship_holder"])
     df["Debtor"] = encoder_Debtor.transform(data["Debtor"])
-    df["Displaced"] = encoder_Displaced.transform(data["Displaced"])
-    df["Daytime_evening_attendance"] = encoder_Daytime_evening_attendance.transform(data["Daytime_evening_attendance"])
     df["Gender"] = encoder_Gender.transform(data["Gender"])
+    df["Tuition_fees_up_to_date"] = encoder_Tuition_fees_up_to_date.transform(data["Tuition_fees_up_to_date"])
+    df["Displaced"] = encoder_Displaced.transform(data["Displaced"])
+    df["Scholarship_holder"] = encoder_Scholarship_holder.transform(data["Scholarship_holder"])
+    df["Daytime_evening_attendance"] = encoder_Daytime_evening_attendance.transform(data["Daytime_evening_attendance"])
+    df["Previous_qualification_grade"] = scaler_Previous_qualification_grade.transform(np.asarray(data["Previous_qualification_grade"]).reshape(-1, 1))
     df["Admission_grade"] = scaler_Admission_grade.transform(np.asarray(data["Admission_grade"]).reshape(-1, 1))
     df["Curricular_units_1st_sem_approved"] = scaler_Curricular_units_1st_sem_approved.transform(np.asarray(data["Curricular_units_1st_sem_approved"]).reshape(-1, 1))
     df["Curricular_units_1st_sem_credited"] = scaler_Curricular_units_1st_sem_credited.transform(np.asarray(data["Curricular_units_1st_sem_credited"]).reshape(-1, 1))
@@ -49,6 +50,5 @@ def data_preprocessing(data):
     df["Curricular_units_2nd_sem_credited"] = scaler_Curricular_units_2nd_sem_credited.transform(np.asarray(data["Curricular_units_2nd_sem_credited"]).reshape(-1, 1))
     df["Curricular_units_2nd_sem_enrolled"] = scaler_Curricular_units_2nd_sem_enrolled.transform(np.asarray(data["Curricular_units_2nd_sem_enrolled"]).reshape(-1, 1))
     df["Curricular_units_2nd_sem_grade"] = scaler_Curricular_units_2nd_sem_grade.transform(np.asarray(data["Curricular_units_2nd_sem_grade"]).reshape(-1, 1))
-    df["Previous_qualification_grade"] = scaler_Previous_qualification_grade.transform(np.asarray(data["Previous_qualification_grade"]).reshape(-1, 1))
     
     return df
